563d30ffb5a3463f55459282eeb4c2e61466e780
Kazai777 <zakaria.chaikhi.pro@gmail.com>
2025-02-05 15:27:20 +0100

    add gnoDebugFactory.ts for create new session debugger

--- extension/src/gnoDebugFactory.ts
+++ extension/src/gnoDebugFactory.ts
@@ -1,21 +1,13 @@
 import * as vscode from 'vscode';
+import { GnoDebugSession } from './debugAdapter/gnoDebug';
 
-export class GnoDebugAdapterDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
-	constructor(private outputChannel: vscode.OutputChannel) {}
-	public createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
-		// if use extern DAP server
-		if (session.configuration.remote) {
-			const host = session.configuration.host || '127.0.0.1';
-			const port = session.configuration.port || 2345;
-			this.outputChannel.appendLine(`Connecting to Gno debug server at ${host}:${port}`);
-			return new vscode.DebugAdapterServer(port, host);
-		}
-
-		// if use intern adapter
-		const command = 'node';
-		const args = [vscode.extensions.getExtension('gno.debug')!.extensionPath + '/out/debugAdapter.js'];
-		this.outputChannel.appendLine(`Starting Gno debug adapter: ${command} ${args.join(' ')}`);
-
-		return new vscode.DebugAdapterExecutable(command, args);
-	}
+export class GnoDebugAdapterDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {    
+    createDebugAdapterDescriptor(
+        session: vscode.DebugSession,
+        executable: vscode.DebugAdapterExecutable | undefined
+    ): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
+        // Create a new session each time
+        const debugSession = new GnoDebugSession();
+        return new vscode.DebugAdapterInlineImplementation(debugSession);
+    }
 }
