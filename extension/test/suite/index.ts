ddc6825702f1babcef677c0b88ee54f3b71dd5dc
aeddi <antoine.e.b@gmail.com>
2025-08-01 20:34:00 +0200

    test: bootstrap basic tests setup

---
+++ extension/test/suite/index.ts
@@ -0,0 +1,38 @@
+import * as path from 'path';
+const glob = require('glob');
+
+export function run(): Promise<void> {
+	// Create the mocha test
+	const Mocha = require('mocha');
+	const mocha = new Mocha({
+		ui: 'tdd',
+		color: true
+	});
+
+	const testsRoot = path.resolve(__dirname, '..');
+
+	return new Promise((resolve, reject) => {
+		glob('**/**.test.js', { cwd: testsRoot }, (err: any, files: string[]) => {
+			if (err) {
+				return reject(err);
+			}
+
+			// Add files to the test suite
+			files.forEach((f: string) => mocha.addFile(path.resolve(testsRoot, f)));
+
+			try {
+				// Run the mocha test
+				mocha.run((failures: number) => {
+					if (failures > 0) {
+						reject(new Error(`${failures} tests failed.`));
+					} else {
+						resolve();
+					}
+				});
+			} catch (err) {
+				console.error(err);
+				reject(err);
+			}
+		});
+	});
+}
